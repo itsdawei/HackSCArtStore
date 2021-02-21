bf672b55163dd56f28739a1d239d2a86fb6d3aba
David Lee <dhlee@usc.edu>
2021-02-21 10:54:05 +0800

    coverted Promise chain to aync-await

--- send-money.js
+++ send-money.js
@@ -1,6 +1,9 @@
+const BigNumber = require("bignumber.js");
+
 const myAddress = "rQqvCc8TMsFDqRQuZGe2ksSvdsZMmmJ3o";
 const mySecret = "shZN3zqASwKQ8EwXCPmn9hMQc8H5j";
 const myDest = "rpeUYxTdoGppcVnwFcepnr9TFf3bpqWhQX";
+const myCurrency = "EUR";
 
 const RippleAPI = require("ripple-lib").RippleAPI;
 const api = new RippleAPI({
@@ -8,7 +11,7 @@ const api = new RippleAPI({
   // by Ripple, Inc.
 });
 
-function sendMoney(myAddress, mySeret, myDest) {
+async function sendMoney(myAddress, mySeret, myDest) {
   api.on("error", (errorCode, errorMessage) => {
     console.log(errorCode + ": " + errorMessage);
   });
@@ -21,41 +24,52 @@ function sendMoney(myAddress, mySeret, myDest) {
     // by the server will be 1000 if this was normal closure
     console.log("disconnected, code:", code);
   });
-  api
-    .connect()
-    .then(() => {
-      enableRippling(myAddress, mySecret);
-    })
-    .then(() => {
-      openTrustline(myAddress, mySecret, myDest, "EUR");
-    })
-    .then(() => {
-      const preparedTx = api.prepareTransaction({
-        TransactionType: "Payment",
-        Account: myAddress,
-        Amount: "2000000",
-        Destination: myDest,
-      });
-      return preparedTx;
-    })
-        .then((pTx) => {
-    const response = api.sign(pTx.txJSON, mySecret);
-    const txID = response.id;
-    console.log("Identifying hash:", txID);
-    const txBlob = response.signedTransaction;
-    console.log("Signed blob:", txBlob);
-    return txBlob
-  })
-  .then((txBlob) => {
-    const result = api.submit(txBlob)
-    console.log("Tentative result code:", result.resultCode)
-    console.log("Tentative result message:", result.resultMessage)
+  
+  try {
+    await api.connect();
+    await enableRippling(myAddress,mySecret);
+    await openTrustline(myAddress, mySecret, myDest, myCurrency);
+    await issueTokens(myAddress, mySecret, myDest, myCurrency, "1", 0.1);
+    let pTx = await doPrepare(myAddress, myDest);
+    let txBlob = await signTransaction(pTx, mySecret);
+    await doSubmit(txBlob);
+    await api.disconnect();
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-  })
-    .then(() => {
-      return api.disconnect();
-    })
-    .catch(console.error);
+async function doPrepare(address, destination) {
+  const preparedTx = await api.prepareTransaction({
+    TransactionType: "Payment",
+    Account: myAddress,
+    Amount: "2000000",
+    Destination: myDest,
+  });
+  return preparedTx;
+}
+
+function signTransaction(pTx, mySecret) {
+  const response = api.sign(pTx.txJSON, mySecret);
+  const txID = response.id;
+  console.log("Identifying hash:", txID);
+  const txBlob = response.signedTransaction;
+  console.log("Signed blob:", txBlob);
+  return txBlob
+}
+
+async function doSubmit(txBlob) {
+  const latestLedgerVersion = await api.getLedgerVersion()
+
+  const result = await api.submit(txBlob)
+
+  console.log("Tentative result code:", result.resultCode)
+  console.log("Tentative result message:", result.resultMessage)
+
+  // Return the earliest ledger index this transaction could appear in
+  // as a result of this submission, which is the first one after the
+  // validated ledger at time of submission.
+  return latestLedgerVersion + 1 
 }
 
 async function enableRippling(genesisAddress, genesisSecret) {
@@ -68,20 +82,57 @@ async function enableRippling(genesisAddress, genesisSecret) {
   console.log("Submitted Set Default Ripple", submittedSettings);
 }
 
-async function openTrustline(sourceAddress, sourceSecret, genesisAddress, currency){
+async function openTrustline(
+  sourceAddress,
+  sourceSecret,
+  genesisAddress,
+  currency
+) {
   const preparedTrustline = await api.prepareTrustline(sourceAddress, {
     currency,
     counterparty: genesisAddress,
-    limit: '100000',
+    limit: "100000",
     ripplingDisabled: false,
-  })
+  });
 
   const signature = api.sign(preparedTrustline.txJSON, sourceSecret)
-    .signedTransaction
+    .signedTransaction;
 
-  const submitResponse = await api.submit(signature)
+  const submitResponse = await api.submit(signature);
 
-  console.log('Trustline Submit Response', submitResponse)
+  console.log("Trustline Submit Response", submitResponse);
 }
 
-sendMoney(myAddress, mySecret, myDest)
+async function issueTokens(
+  genesisAddress,
+  genesisSecret,
+  destinationAddress,
+  currency,
+  value
+) {
+  const preparedTokenIssuance = await api.preparePayment(genesisAddress, {
+    source: {
+      address: genesisAddress,
+      maxAmount: {
+        value: value,
+        currency,
+        counterparty: genesisAddress,
+      },
+    },
+    destination: {
+      address: destinationAddress,
+      amount: {
+        value: value,
+        currency,
+        counterparty: genesisAddress,
+      },
+    },
+  });
+  const issuanceResponse = await api.submit(
+    api.sign(preparedTokenIssuance.txJSON, genesisSecret).signedTransaction
+  );
+
+  console.log("Issuance Submission Response", issuanceResponse);
+}
+
+sendMoney(myAddress, mySecret, myDest);
